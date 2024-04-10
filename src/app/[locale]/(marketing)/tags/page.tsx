8180737567e487a7ce3b7de546f30d9d44f8dc0b
Piotr Sierant <psierant@multiprojekt.pl>
2024-04-10 13:45:36 +0200

    tagi

---
+++ src/app/[locale]/(marketing)/tags/page.tsx
@@ -0,0 +1,65 @@
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server'
+import { CustomLink } from '@/components/Link'
+import Tag from '@/components/Tag'
+import { slug } from 'github-slugger'
+import { genPageMetadata } from "@/app/seo";
+import { getLocalePrimaryDialects } from '@/data/locales'
+import { getTagsMeta } from '@/lib/tags'
+
+export const revalidate = 3600;
+
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
+  const t = await getTranslations({ locale, namespace: "Tags" });
+  const title = t('title');
+  const description = t('desc');
+  const keywords = t('keywords');
+  const localeShort = getLocalePrimaryDialects(locale);
+  const obj = {
+    title,
+    description,
+    keywords,
+    localeShort,
+  }
+  const meta = genPageMetadata(obj)
+  return meta
+}
+
+export default async function TagsPage({ params: { locale } }: { params: { locale: string } }) {
+  unstable_setRequestLocale(locale);
+  const t = await getTranslations('Tags');
+  let tags = await getTagsMeta(locale) as Record<string, number>;
+  if (tags == undefined) {
+    tags = {}
+  }
+  const tagKeys = Object.keys(tags)
+  const sortedTags = tagKeys.sort((a, b) => tags[b] - tags[a])
+
+  return (
+    <>
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+            {t("tags")}
+          </h1>
+        </div>
+        <div className="flex max-w-lg flex-wrap">
+          {tagKeys.length === 0 &&  t("notfoundTag")}
+          {sortedTags.map((t) => {
+            return (
+              <div key={t} className="mb-2 mr-5 mt-2">
+                <Tag text={t} />
+                <CustomLink
+                  href={`/tags/${slug(t)}`}
+                  className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                  aria-label={`View posts tagged ${t}`}
+                >
+                  {` (${tags[t]})`}
+                </CustomLink>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
