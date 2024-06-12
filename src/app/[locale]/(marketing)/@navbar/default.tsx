f221225a4f8b672ead40f70fa12005a9a67a5373
Piotr Sierant <psierant@multiprojekt.pl>
2024-06-12 13:07:41 +0200

    with undefined

--- src/app/[locale]/(marketing)/@navbar/default.tsx
+++ src/app/[locale]/(marketing)/@navbar/default.tsx
@@ -1,14 +1,14 @@
 import { ComponentSearch } from "@/components/ComponentSearch";
 import { UserAccountNav } from "@/components/UserAccountNav";
 import HeaderUser from "@/components/auth/NavbarUser";
-import { getCurrentUser } from "@/utils/session";
+// import { getCurrentUser } from "@/utils/session";
 
-export default async function Navbar() {
-    const user = await getCurrentUser();
+export default function Navbar() {
+    // const user = await getCurrentUser();
     return (
             <ComponentSearch>
                 <HeaderUser>
-                    <UserAccountNav user={user} />
+                    <UserAccountNav user={undefined} />
                 </HeaderUser>
             </ComponentSearch>
     )
