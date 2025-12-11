c6deb97c78f41f3e989678e3e1dcafd47df7ead4
Martin Szerment <szerment84@gmail.com>
2025-12-11 12:47:12 +0100

    Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,7 +30,7 @@ export const Footer = () => {
         >
           <Image
             src={Logo.src}
-            alt={"OmniMES logo"}
+            alt="OmniMES logo"
             width={44}
             height={36}
             className="z-1 mr-2"
@@ -38,10 +38,12 @@ export const Footer = () => {
           />
           <span>OmniMES</span>
         </a>
+
         <DescriptionSecondary text={t("title")} />
+
         <ul className="my-6 flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
           <li>
-            <a href={"/"} className="mr-4 hover:underline md:mr-6">
+            <a href="/" className="mr-4 hover:underline md:mr-6">
               {tl("home")}
             </a>
           </li>
@@ -53,8 +55,30 @@ export const Footer = () => {
             </li>
           ))}
         </ul>
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023-2024{" "}
+
+        <div className="mt-3 flex justify-center items-center space-x-2 text-gray-900 dark:text-white">
+          <a
+            href="https://x.com/OmnimesOfficial"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center hover:text-blue-500 transition-colors"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="mr-1"
+            >
+              <path d="M18.244 2.25h3.308l-7.227 8.26L24 21.75h-6.828l-5.316-6.977-6.087 6.977H2.46l7.73-8.86L0 2.25h7.078l4.843 6.41 6.323-6.41z" />
+            </svg>
+            <span>x.com/OmnimesOfficial</span>
+          </a>
+        </div>
+
+        <span className="mt-4 block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2021–{new Date().getFullYear()}{" "}
           <a href="/" className="hover:underline">
             OmniMES™
           </a>
